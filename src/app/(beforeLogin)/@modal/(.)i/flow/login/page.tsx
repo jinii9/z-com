8eedfb540f7bfa04514dc3c4733791b2a2e5aca7
jinii9 <jinii915@naver.com>
2024-07-11 23:29:33 +0900

    section2.

--- src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -3,8 +3,8 @@ import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 export default function Page() {
   return (
     <>
-    난 가로채기지롱 ㅋㅋ
-     <LoginModal />
+      난 가로채기지롱 ㅋㅋ
+      <LoginModal />
     </>
-  )
+  );
 }
--- src/app/(beforeLogin)/i/flow/signup/page.tsx
+++ src/app/(beforeLogin)/i/flow/signup/page.tsx
@@ -1,7 +1,7 @@
-import SignupModal from '@/app/(beforeLogin)/_component/SignupModal';
+import SignupModal from "@/app/(beforeLogin)/_component/SignupModal";
 
-export default function Home() {
-    return(
-        <SignupModal />
-    )
+export default function Signup() {
+  return (
+    <SignupModal />
+  )
 }
